c1ec33b94e37aab92b8fbda7019e2e5e7405931c
sameekshamohanty <78748985+sameekshamohanty@users.noreply.github.com>
2022-02-22 18:59:32 +0530

    Create q1.java

---
+++ q1.java
@@ -0,0 +1,12 @@
+public class q1 
+{
+		 
+	 public static void main(String[] args) 
+	    {
+	        String str = "Hello World";
+	        int c = str.length();
+	        System.out.println("The String has " +c +" characters");
+	 
+	    }
+	 
+}
